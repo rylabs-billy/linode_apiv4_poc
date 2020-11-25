6e96c6640b1fbe8959c5e007684701bc2b78c280
Billy Thompson <bthompson@linode.com>
2020-11-25 16:09:59 -0500

    more realistic

--- shell_example.sh
+++ shell_example.sh
@@ -3,51 +3,58 @@
 # sudo apt install jq -y
 
 # create linode and check status
-linode_id=$(curl -sH "Content-Type: application/json" \
-    -H "Authorization: Bearer $TOKEN" \
-    -X POST -d '{
-      "backups_enabled": true,
-      "swap_size": 512,
-      "image": "linode/debian9",
-      "root_pass": "aComplexP@ssword",
-      "authorized_keys": [
-        "<SSH_KEY>"
-      ],
-      "booted": true,
-      "label": "shell_example",
-      "type": "g6-nanode-1",
-      "region": "ap-southeast"
-    }' \
-    https://api.linode.com/v4/linode/instances | jq -r .id)
+create_linode () {
+  linode_id=$(curl -sH "Content-Type: application/json" \
+      -H "Authorization: Bearer $TOKEN" \
+      -X POST -d '{
+        "backups_enabled": true,
+        "swap_size": 512,
+        "image": "linode/debian9",
+        "root_pass": "aComplexP@ssword",
+        "authorized_keys": [
+          "SSH_KEY"
+        ],
+        "booted": true,
+        "label": "shell_example",
+        "type": "g6-nanode-1",
+        "region": "ap-southeast"
+      }' \
+      https://api.linode.com/v4/linode/instances | jq -r .id)
 
-while true; do
-  linode_status=$(curl -sH "Authorization: Bearer $TOKEN" https://api.linode.com/v4/linode/instances/$linode_id | jq -r .status)
-  sleep 1
-  if [[ $linode_status == "running" ]]; then
-    break
-  fi
-done
+  while true; do
+    linode_status=$(curl -sH "Authorization: Bearer $TOKEN" https://api.linode.com/v4/linode/instances/$linode_id | jq -r .status)
+    sleep 1
+    if [[ $linode_status == "running" ]]; then
+      break
+    fi
+  done
 
-echo "Linode is $linode_status."
-sleep 10
+  echo "Linode is $linode_status."
+}
 
 # resize disk and check status
-disk_id=$(curl -sH "Authorization: Bearer $TOKEN" https://api.linode.com/v4/linode/instances/$linode_id/disks/ | jq -r .data[0].id)
+resize_disk () {
+  disk_id=$(curl -sH "Authorization: Bearer $TOKEN" https://api.linode.com/v4/linode/instances/$linode_id/disks/ | jq -r .data[0].id)
 
-curl -sH "Content-Type: application/json" \
-    -H "Authorization: Bearer $TOKEN" \
-    -X POST -d '{
-      "size": 15000
-    }' \
-    https://api.linode.com/v4/linode/instances/$linode_id/disks/$disk_id/resize
+  curl -sH "Content-Type: application/json" \
+      -H "Authorization: Bearer $TOKEN" \
+      -X POST -d '{
+        "size": 15000
+      }' \
+      https://api.linode.com/v4/linode/instances/$linode_id/disks/$disk_id/resize
 
-while true; do
-  disk_status=$(curl -sH "Authorization: Bearer $TOKEN" https://api.linode.com/v4/linode/instances/$linode_id/disks/$disk_id | jq -r .status)
-  sleep 1
-  if [[ $disk_status == "ready" ]]; then
-    break
-  fi
-done
+  while true; do
+    disk_status=$(curl -sH "Authorization: Bearer $TOKEN" https://api.linode.com/v4/linode/instances/$linode_id/disks/$disk_id | jq -r .status)
+    sleep 1
+    if [[ $disk_status == "ready" ]]; then
+      break
+    fi
+  done
 
-echo "Disk is $disk_status."
+  echo "Disk is $disk_status."
+}
 
+# main
+create_linode
+sleep 10
+resize_disk
